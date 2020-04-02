6da807f8cf562818fbc2024cc42518fd1bd336c9
Anna Chumakova <chumakova3008@gmail.com>
2020-04-03 01:56:57 +0500

    сделан createRequest

--- js/api/createRequest.js
+++ js/api/createRequest.js
@@ -3,5 +3,40 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+	const xhr = new XMLHttpRequest();
+	const formData = new FormData;
+	xhr.withCredentials = true;
+	xhr.responseType = "json";
+	let urlXhr;
 
+	if (options.method === "GET") {
+		urlXhr = `${options.url}?mail=${options.data.mail}&password${options.data.password}`;
+	} else {
+		for (let key in options.data) {
+			formData.append(key, options.data[key]);
+		}
+
+		urlXhr = options.url;
+	}
+
+	xhr.addEventListener("load", () => {
+		if (xhr.status != 200) {
+			options.callback(err, xhr.response)
+			console.log(err);
+		} else {
+			options.callback(null, xhr.response)
+			console.log(xhr.response);
+		}
+	});
+
+	xhr.open(options.method, urlXhr);
+	
+	try {
+		xhr.send(formData);
+	}
+	catch (err) {
+		callback(err);
+	}
+
+	return xhr;
 };
